f6c5ef910ff46c54ffeda722861032a131fd91a0
Thomas Darimont <thomas.darimont@gmail.com>
2015-01-30 21:00:46 +0100

    Add support for dynamically attaching the allocation-tracker-agent.
    Introduced an agentmain method and enabling retransform support for the agent allows to dynamically attach the allocation-tracker.
    
    The following is an example for dynamically attaching a the agent locally.
    
    The Application that produces some garbage: AllocationTrackerExample
    ```java
    package de.tutorials.training;
    
    import java.lang.management.ManagementFactory;
    import java.util.Date;
    import java.util.concurrent.Executors;
    import java.util.concurrent.TimeUnit;
    
    /**
     * @author Thomas Darimont
     */
    public class AllocationTrackerExample {
    
        public static void main(String[] args) throws Exception {
    
            System.out.println(ManagementFactory.getRuntimeMXBean().getName());
    
            System.out.println("Started");
    
            Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> new Grabage(), 1, 2, TimeUnit.SECONDS);
        }
    
        static class Grabage {
            Grabage() {
                System.out.println("Created new Garbage@" + new Date());
            }
        }
    }
    ```
    
    The AgentAttacher helper performs the dynamic attachment of the agent programmatically by using classes
    from JDK tools.jar.
    
    run with the following arguments:
    $PATH_TO_AGENT_JAR% %PID_OF_ATTACH_TARGET% %PACKAGE_PREFIX%
    
    e.g.:
    ```
    "D:\development\java\repos\git\github\allocation-tracker\target\allocation-tracker-agent-0.0.1-SNAPSHOT.jar" 5012 de.tutorials.training
    ```
    
    ```java
    package de.tutorials.training;
    
    import com.sun.tools.attach.VirtualMachine;
    
    /**
     * @author Thomas Darimont
     */
    public class AgentAtacher {
    
        public static void main(String[] args) {
    
            assert args.length == 3;
    
            String agentPath = args[0];
            String pid = args[1];
            String packages = args[2];
    
            try {
                VirtualMachine vm = VirtualMachine.attach(pid);
                vm.loadAgent(agentPath, packages);
                vm.detach();
            } catch (Exception e) {
                throw new RuntimeException(e);
            }
        }
    }
    ```

--- src/main/java/de/codecentric/performance/agent/allocation/AllocationProfilingAgent.java
+++ src/main/java/de/codecentric/performance/agent/allocation/AllocationProfilingAgent.java
@@ -4,10 +4,11 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
+import javax.management.*;
 
 import de.codecentric.performance.agent.allocation.mbean.Agent;
 
@@ -15,6 +16,16 @@ import de.codecentric.performance.agent.allocation.mbean.Agent;
  * Class registered as premain hook, will add a ClassFileTransformer and register an MBean for controlling the agent.
  */
 public class AllocationProfilingAgent {
+
+    private static final ObjectName AGENT_OBJECT_NAME;
+
+    static{
+        try {
+            AGENT_OBJECT_NAME = new ObjectName("de.codecentric:type=Agent");
+        } catch (MalformedObjectNameException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
   public static void premain(String agentArgs, Instrumentation inst) {
     String prefix = agentArgs;
@@ -29,7 +40,42 @@ public class AllocationProfilingAgent {
       return;
     }
     registerMBean();
-    inst.addTransformer(new AllocationTrackerClassFileTransformer(prefix));
+    inst.addTransformer(new AllocationTrackerClassFileTransformer(prefix), true);
+  }
+
+ /**
+  * Class registered as agentmain hook, will add a ClassFileTransformer and register an MBean for controlling the agent.
+  */
+ @SuppressWarnings("unused")
+  public static void agentmain(String args, Instrumentation inst) throws Exception {
+
+      MBeanServer mbs = tryGetMbeanServer();
+
+      if(mbs != null){
+          try {
+              mbs.getMBeanInfo(AGENT_OBJECT_NAME);
+              //agent already installed, do nothing
+          }catch(InstanceNotFoundException infe){
+              //install new agent
+              AgentLogger.log("Trying to attach agent dynamically...");
+              premain(args, inst);
+              AgentLogger.log("Attached agent successfully.");
+              if(inst.isRetransformClassesSupported()){
+                  AgentLogger.log("Trying to redefine existing classes...");
+
+                  List<Class> classes = new ArrayList<>(128);
+                  Class[] candidates = inst.getAllLoadedClasses();
+                  for (int i = 0; i < candidates.length; i++) {
+                      if(candidates[i].getName().startsWith(args)) {
+                          classes.add(candidates[i]);
+                      }
+                  }
+
+                  inst.retransformClasses(classes.toArray(new Class[classes.size()]));
+                  AgentLogger.log("Redefine completed.");
+              }
+          }
+      }
   }
 
   /*
@@ -41,17 +87,10 @@ public class AllocationProfilingAgent {
       public void run() {
         try {
           // retry up to a maximum of 10 minutes
-          int retryLimit = 60;
-          MBeanServer mbs = null;
-          while (mbs == null) {
-            if (retryLimit-- == 0) {
-              AgentLogger.log("Could not register Agent MBean in 10 minutes.");
-              return;
-            }
-            TimeUnit.SECONDS.sleep(10);
-            mbs = ManagementFactory.getPlatformMBeanServer();
-          }
-          mbs.registerMBean(new Agent(), new ObjectName("de.codecentric:type=Agent"));
+            MBeanServer mbs = tryGetMbeanServer();
+            if (mbs == null) return;
+
+            mbs.registerMBean(new Agent(), AGENT_OBJECT_NAME);
           AgentLogger.log("Registered Agent MBean.");
         } catch (Exception e) {
           AgentLogger.log("Could not register Agent MBean. Exception:");
@@ -61,6 +100,23 @@ public class AllocationProfilingAgent {
         }
       }
     };
+    thread.setDaemon(true);
     thread.start();
   }
+
+    private static MBeanServer tryGetMbeanServer() throws InterruptedException {
+
+        int retryLimit = 60;
+        MBeanServer mbs = null;
+        while (mbs == null) {
+          if (retryLimit-- == 0) {
+            AgentLogger.log("Could not register Agent MBean in 10 minutes.");
+              return null;
+          }
+          TimeUnit.SECONDS.sleep(10);
+          mbs = ManagementFactory.getPlatformMBeanServer();
+        }
+
+        return mbs;
+    }
 }
